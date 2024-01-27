3e8f50790c941d9fc86b8f1d76143afbe86c9f55
lava_frai <lavafrai@yandex.ru>
2024-01-28 00:22:54 +0300

    Docker done

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
-docker stop ruina_counter
-docker rm ruina_counter
-docker run -it -d -v $(pwd):/opt/app --restart=always -p 8081:80 --name=ruina_counter $(docker build -q .)
+docker stop personal_landing
+docker rm personal_landing
+docker run -it -d -v $(pwd):/opt/app --restart=always -p 8081:80 --name=personal_landing $(docker build -q .)
