ef679e1ac62ed75561d5f5b1226ab66bde710ce5
lavafrai <79652972+lavafrai@users.noreply.github.com>
2025-04-03 10:44:20 +0000

    maiapp release mocks

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route('/donations')
+def donations():
+    return app.redirect(location='/#donation', code=302)
+
+@app.route('/thanks/maiapp')
+def donations():
+    return render_template('thanks/maiapp.html')
 
 @app.route('/<path:path>')
 def send_static(path):
@@ -16,4 +23,4 @@ def send_static(path):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=3000, debug=False)
