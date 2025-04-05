9b27539ab8ff612ae5eabaa38a288be80abd73ba
lavafrai <79652972+lavafrai@users.noreply.github.com>
2025-04-05 23:39:46 +0300

    Update rundocker.sh

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 docker stop personal_landing
 docker rm personal_landing
-docker run -it -d -v $(pwd):/opt/app --restart=always -p 8082:3000 --name=personal_landing $(docker build -q .)
+docker run -it -d -v $(pwd):/opt/app --restart=always -p 80:3000 --name=personal_landing $(docker build -q .)
